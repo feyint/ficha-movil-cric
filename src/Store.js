ddfe357476a293656a4aa28b37d8547071d9b9ca
Luis Felipe Roa Rosero <felipperoan@gmail.com>
2020-08-15 20:44:58 -0500

    delete test

--- src/Store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import Reducers from './Reducers';
-import thunk from 'redux-thunk';
-
-export default Store = () => {
-  let storeApp = createStore(Reducers, applyMiddleware(thunk));
-  return storeApp;
-};
