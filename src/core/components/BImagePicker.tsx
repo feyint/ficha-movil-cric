ea2b8a7850c0cb0c196a1da528076ba21efded54
Milton <tomilton@hotmail.com>
2020-09-14 18:02:17 -0500

    Display Images

--- src/core/components/BImagePicker.tsx
+++ src/core/components/BImagePicker.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { HelperText } from 'react-native-paper';
-import { View, StyleSheet, Modal, Text } from 'react-native';
+import { View, StyleSheet, Modal, Text, TouchableOpacity, Image, Dimensions, FlatList } from 'react-native';
 import { Button } from 'react-native-paper';
 import ImagePicker from 'react-native-image-picker';
 
@@ -47,14 +47,18 @@ class BImagePicker extends Component<Props, State> {
             console.log('Uri=', response.uri);
             if (response.didCancel) {
                 console.log("User cancelled image picker");
+                this.setState({ modal: false });
             } else if (response.error) {
                 console.log("Image Picker Error", response.error);
             } else {
-                const uri = response.uri
-                const type = "image/jpg"
-                const name = response.fileName
-                const source = { uri, type, name }
+                const path = `file:///${response.path}`;
+                const type = "image/jpg";
+                const name = response.fileName;
+                const source = { path, type, name };
                 console.log('Camera: ', source)
+                this.state.images.push(source);
+                this.setState({ images: this.state.images });
+                this.setState({ modal: false });
             }
         })
     }
@@ -68,17 +72,20 @@ class BImagePicker extends Component<Props, State> {
             }
         }
         ImagePicker.launchImageLibrary(options, (response) => {
-            console.log('Response=', response);
+            // console.log('Response=', response);
             if (response.didCancel) {
                 console.log("User cancelled image picker");
             } else if (response.error) {
                 console.log("Image Picker Error", response.error);
             } else {
-                const uri = response.uri
+                const path = `file:///${response.path}`
                 const type = "image/jpg"
                 const name = response.fileName
-                const source = { uri, type, name }
+                const source = { path, type, name }
                 console.log('ImageLibrary: ', source)
+                this.state.images.push(source);
+                this.setState({ images: this.state.images });
+                this.setState({ modal: false });
             }
         })
     }
@@ -93,6 +100,30 @@ class BImagePicker extends Component<Props, State> {
         );
     }
 
+    itemSeparator = () => { return this.horizontalSeparator() }
+
+    horizontalSeparator() {
+        return (
+            <View style={stylesSeparator.separatorHorizontal} />
+        );
+    }
+
+    Image(image: any) {
+        console.log('Image: ', image);
+        return (
+            <View style={stylesDisplayImages.container}>
+                <View>
+                    <Image style={stylesDisplayImages.cover}
+                        source={{ uri: image.path }} />
+                </View>
+                <View style={stylesDisplayImages.delete}>
+                    <Button color="red" icon="trash-can-outline" mode="text" onPress={() => console.log('Delete')}>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+
     render() {
         return (
             <View>
@@ -101,6 +132,12 @@ class BImagePicker extends Component<Props, State> {
                     <Button icon="upload" style={styles.buttonUpload} labelStyle={styles.text} mode="outlined" onPress={() => this.setState({ modal: true })}>
                         Adjuntar
                     </Button>
+
+                    <FlatList
+                        horizontal
+                        ItemSeparatorComponent={this.itemSeparator}
+                        data={this.state.images}
+                        renderItem={({ item }) => this.Image(item)} />
 
                     <Modal
                         animationType='slide'
@@ -122,26 +159,47 @@ class BImagePicker extends Component<Props, State> {
                                 Cancelar
                             </Button>
                         </View>
-
                     </Modal>
                 </View>
-                {this.props.error ? (
-                    <HelperText type="error">
-                        {this.props.errorText ? this.props.errorText : 'Campo invalido'}
-                    </HelperText>
-                ) : null}
-            </View>
+                {
+                    this.props.error ? (
+                        <HelperText type="error">
+                            {this.props.errorText ? this.props.errorText : 'Campo invalido'}
+                        </HelperText>
+                    ) : null
+                }
+            </View >
         );
     }
-
 }
-const styles = StyleSheet.create({
-    pickedImageWrapper: {
-        height: 20 / 2,
-    },
+
+const stylesSeparator = StyleSheet.create({
+    separatorHorizontal: {
+        flex: 1,
+        marginHorizontal: 5,
+        borderTopColor: 'red'
+    }
+})
+
+const stylesDisplayImages = StyleSheet.create({
     container: {
-        flex: 1
+        flexDirection: 'row'
     },
+    cover: {
+        height: 150,
+        width: 100,
+        resizeMode: 'contain'
+    },
+    delete: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        paddingVertical: 5,
+        paddingHorizontal: 7
+    },
+});
+
+const styles = StyleSheet.create({
     input: {
         margin: 6,
     },
@@ -160,13 +218,14 @@ const styles = StyleSheet.create({
     },
     buttonUpload: {
         width: '100%',
-
+        marginBottom: 5
     },
     text: {
         fontWeight: 'bold',
         fontSize: 15,
         lineHeight: 26,
-    }
+    },
+
 });
 
 export default BImagePicker;
